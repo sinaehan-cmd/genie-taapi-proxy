539eef2fefc2c63ee0c3947800c2d2b54b991c51
sinaehan-cmd <seseragi.sh@gmail.com>
2025-11-22 17:08:45 +0900

    Update dominance_routes.py

--- genie_server/routes/dominance_routes.py
+++ genie_server/routes/dominance_routes.py
@@ -1,10 +1,62 @@
-from flask import Blueprint, request, jsonify
-from genie_server.utils.dominance_fetcher import get_dominance
+# genie_server/utils/dominance_fetcher.py
 
-bp = Blueprint("dominance_routes", __name__)
+import os, json, time
+import requests
 
-@bp.route("/dominance")
-def dominance_api():
-    tf = request.args.get("tf", "1h")
-    data = get_dominance(tf)
-    return jsonify(data)
+DOM_LOG_PATH = "/opt/render/project/src/genie_server/utils/dominance_log.json"
+COINGECKO_URL = "https://api.coingecko.com/api/v3/global"
+
+def get_current_dominance():
+    """현재 BTC dominance 1회 조회"""
+    try:
+        r = requests.get(COINGECKO_URL, timeout=10)
+        data = r.json()
+        return float(data["data"]["market_cap_percentage"]["btc"])
+    except:
+        return None
+
+def load_log():
+    if not os.path.exists(DOM_LOG_PATH):
+        return []
+    try:
+        with open(DOM_LOG_PATH, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_log(log):
+    with open(DOM_LOG_PATH, "w") as f:
+        json.dump(log, f)
+
+def add_snapshot():
+    """30분마다 dominance 스냅샷 저장"""
+    value = get_current_dominance()
+    if value is None:
+        return False
+
+    log = load_log()
+    log.append({"ts": int(time.time()), "dominance": value})
+
+    # 최근 48개만 유지 (24시간)
+    log = log[-48:]
+
+    save_log(log)
+    return True
+
+def get_avg(hours):
+    """최근 N시간 평균 (hours=4 or 24)"""
+    log = load_log()
+    if not log:
+        return None
+
+    need = int((hours * 60) / 30)   # 4h=8개, 24h=48개
+    samples = log[-need:]
+
+    if not samples:
+        return None
+
+    vals = [x["dominance"] for x in samples if "dominance" in x]
+    if not vals:
+        return None
+
+    return round(sum(vals) / len(vals), 2)
