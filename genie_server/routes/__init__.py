893303b16efe8835f0ab81a909d6343d1895db1b
sinaehan-cmd <seseragi.sh@gmail.com>
2025-11-22 15:54:37 +0900

    Update __init__.py

--- genie_server/routes/__init__.py
+++ genie_server/routes/__init__.py
@@ -1,18 +1,28 @@
 # genie_server/routes/__init__.py
 
 from . import (
-    base_routes, view_routes, write_routes,
-    loop_auto, loop_prediction, loop_gti,
-    loop_learning, loop_system, loop_final,
-    loop_auto_gti, mvrv_routes
+    base_routes,
+    view_routes,
+    write_routes,
+    loop_auto,
+    loop_prediction,
+    loop_gti,
+    loop_learning,
+    loop_system,
+    loop_final,
+    loop_auto_gti,
+    mvrv_routes,
 )
 
-# indicator routes – 모듈이 아니라 블루프린트(bp)만 가져옴
+# indicator / dominance는 모듈이 아니라 bp만 직접 가져옴
 from .indicator_routes import bp as bp_indicator
+from .dominance_routes import bp as bp_dominance
 
 
 def register_routes(app):
-    # 다른 라우트 모듈들 등록
+    """모든 Blueprint를 Flask 앱에 등록"""
+
+    # 1) 일반 라우트 모듈들
     for module in [
         base_routes,
         view_routes,
@@ -28,5 +38,6 @@ def register_routes(app):
     ]:
         app.register_blueprint(module.bp)
 
-    # indicator는 개별 등록
+    # 2) 개별 블루프린트 등록 (indicator / dominance)
     app.register_blueprint(bp_indicator)
+    app.register_blueprint(bp_dominance)
