791645be13cfcce259b8b7377f0f67ede73b8da2
sinaehan-cmd <seseragi.sh@gmail.com>
2025-12-02 13:57:36 +0900

    Update loop_routes.py

--- routes/loop_routes.py
+++ routes/loop_routes.py
@@ -1,1 +1,14 @@
+from flask import Blueprint
+from loops.prediction_loop import run_prediction_loop
+from loops.gti_loop import run_gti_loop
+
+loop_bp = Blueprint("loop_routes", __name__)
+
+@loop_bp.route("/run/prediction")
+def run_prediction():
+    return run_prediction_loop()
+
+@loop_bp.route("/run/gti")
+def run_gti():
+    return run_gti_loop()
 
