f5bbc714d124b6462c33907675cbce605f59788b
sinaehan-cmd <seseragi.sh@gmail.com>
2025-12-02 13:55:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from routes.view_routes import view_bp
+from routes.write_routes import write_bp
+from routes.loop_routes import loop_bp
+
+app = Flask(__name__)
+
+# Blueprint 등록
+app.register_blueprint(view_bp)
+app.register_blueprint(write_bp)
+app.register_blueprint(loop_bp)
+
+@app.route("/")
+def home():
+    return "Genie Server v2025.12 — OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
