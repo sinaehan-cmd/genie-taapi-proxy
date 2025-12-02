5ca1a950b78a7f035b7d4f988e15e8e5ba2541bb
sinaehan-cmd <seseragi.sh@gmail.com>
2025-12-02 13:56:24 +0900

    Update market_service.py

--- services/market_service.py
+++ services/market_service.py
@@ -1,1 +1,16 @@
 
+import requests
+
+def get_usd_krw():
+    try:
+        r = requests.get("https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD").json()
+        return r[0]["basePrice"]
+    except:
+        return None
+
+def get_fng_index():
+    try:
+        r = requests.get("https://api.alternative.me/fng/?limit=1").json()
+        return r["data"][0]["value"]
+    except:
+        return None
